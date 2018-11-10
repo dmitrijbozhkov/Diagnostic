e19dbe9b7073bb1341bac51ba9ac786da610a368
Matviei <serbullm@gmail.com>
2018-11-10 21:04:22 +0100

    Login and signup pages (frontend)

--- frontend/app/routes/fuel-catalogue/index.ts
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class FuelCatalogueIndex extends Route.extend({
-  // anything which *must* be merged to prototype here
-}) {
-  model() {
-    return this.store.findAll("fuel/fuel-details");
-  }
-}
